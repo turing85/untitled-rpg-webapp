4a93e97ab6c067c581ddcdf32728ac336c0ede82
sglass <s.glass@intershop.de>
2020-04-02 08:08:18 +0200

    [#RPG-6-basic-layout]:
    remove last component styles

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'rpg-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'frontend';
