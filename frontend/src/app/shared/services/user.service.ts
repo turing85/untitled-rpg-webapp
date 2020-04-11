24472b43509d1879917c99d57d56bc7f3dbcc9ea
Sebastian Glaß <sebastian.glass15@gmail.com>
2020-04-11 10:33:20 +0200

    #RPG-16: add register to user service

--- frontend/src/app/shared/services/user.service.ts
+++ frontend/src/app/shared/services/user.service.ts
@@ -5,7 +5,13 @@ import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { fromKeycloakProfile } from 'src/app/model/user/user.keycload.mapper';
 import { Router } from '@angular/router';
-import {PlatformLocation } from '@angular/common';
+import { PlatformLocation } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+
+
+type LanguageTag = 'en-GB' | 'de-DE';
+
+const USERS_URL = "http://localhost:8081/users"
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +19,9 @@ import {PlatformLocation } from '@angular/common';
 export class UserService {
   constructor(
     private keycloakAngular: KeycloakService,
-    private platformLocation: PlatformLocation
-  ) {}
+    private platformLocation: PlatformLocation,
+    private httpService: HttpClient
+  ) { }
 
   getUserDetails(): Observable<User> {
     const userDetails = this.keycloakAngular.getKeycloakInstance().profile;
@@ -27,5 +34,9 @@ export class UserService {
   logout() {
     const landingPageUrl = (this.platformLocation as any).location.origin;
     this.keycloakAngular.logout(landingPageUrl);
+  }
+
+  register(name: string, email: string, password: string, preferredLanguageTag: LanguageTag): Observable<unknown> {
+    return this.httpService.post(USERS_URL, { name, email, password, preferredLanguageTag });
   }
 }
